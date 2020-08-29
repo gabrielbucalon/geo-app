4c17399fe4092155253015c3e8d6d412f2c1ad09
gabriel.bucalon <bucalongabriell@gmail.com>
2020-08-29 14:45:52 -0300

    feat: add points on map

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,25 @@
 let map;
 
-function initMap() {
-  console.log("olá");
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+async function initMap() {
+  let points = await getPoints();
+
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: { lat: points[0].lat, lng: points[0].lng },
   });
-  return map;
+
+  var image =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+
+  points.forEach((element) => {
+    var beachMarker = new google.maps.Marker({
+      position: { lat: element.lat, lng: element.lng },
+      map: map,
+      icon: image,
+    });
+  });
+}
+
+function getPoints() {
+  return fetch("src/data/points.json").then((response) => response.json());
 }
